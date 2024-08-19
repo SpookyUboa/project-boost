8726b37aa39c92117ca4caa4d27d9b17e763946b
Edu Romero <eduardojrl927@gmail.com>
2024-08-19 20:31:01 +0200

    Thrust audio and particles

--- Script/Player.gd
+++ Script/Player.gd
@@ -10,15 +10,35 @@ var tween = Tween
 
 var is_transitioning : bool = false
 
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var victory_audio: AudioStreamPlayer = $VictoryAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var booster_particles_left: GPUParticles3D = $BoosterParticlesLeft
+@onready var booster_particles_right: GPUParticles3D = $BoosterParticlesRight
+
+
 func _process(delta: float) -> void:
 	if(Input.is_action_pressed("pb_boost")):
 		apply_central_force(basis.y * delta * thrust)
+		booster_particles.emitting = true
+		if(rocket_audio.playing == false):
+			rocket_audio.play()
+	else:
+		rocket_audio.stop()
+		booster_particles.emitting = false
 		
 	if(Input.is_action_pressed("pb_rotate_left")):
 		apply_torque(Vector3(0, 0, torque * delta))
-	
+		booster_particles_right.emitting = true
+	else:
+		booster_particles_right.emitting = false
+			
 	if(Input.is_action_pressed("pb_rotate_right")):
 		apply_torque(Vector3(0, 0, -torque * delta))
+		booster_particles_left.emitting = true
+	else:
+		booster_particles_left.emitting = false
 
 func _on_body_entered(body: Node) -> void:
 	if "Goal" in body.get_groups() and !is_transitioning:
@@ -31,6 +51,7 @@ func _on_body_entered(body: Node) -> void:
  
 func crash_sequence() -> void: 
 	print("Absolutely smacked")
+	explosion_audio.play()
 	set_process(false)
 	tween = create_tween()
 	tween.tween_interval(2.0)
@@ -38,6 +59,7 @@ func crash_sequence() -> void:
 	
 func complete_level(next_level : String) -> void:
 	print("Landed safely!!")
+	victory_audio.play()
 	tween = create_tween()
 	tween.tween_interval(2.0)
 	tween.tween_callback(
